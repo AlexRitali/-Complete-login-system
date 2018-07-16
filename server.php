a2dfd35a95b4a24b181a3a53daf0f9d3ffb13913
OMAR AL <35746104+OmarAlareeki@users.noreply.github.com>
2018-07-15 23:14:52 -0700

    Update server.php

--- server.php
+++ server.php
@@ -7,7 +7,8 @@
 	$errors = array(); 
 	$_SESSION['success'] = "";
 
-	// connect to database
+	// connect to database through my sql
+        //  name of local host, username, password if there is any, and registeration.
 	$db = mysqli_connect('localhost', 'omaralareeki', '', 'registration');
 
 	// REGISTER USER
@@ -27,7 +28,7 @@
 			array_push($errors, "The two passwords do not match");
 		}
 
-		// register user if there are no errors in the form
+		// register user if there are no errors in the form enteries
 		if (count($errors) == 0) {
 			$password = md5($password_1);//encrypt the password before saving in the database
 			$query = "INSERT INTO users (username, email, password) 
@@ -43,7 +44,7 @@
 
 	// ... 
 
-	// LOGIN USER
+	// LOGIN USER ( after having the user in db name and password)
 	if (isset($_POST['login_user'])) {
 		$username = mysqli_real_escape_string($db, $_POST['username']);
 		$password = mysqli_real_escape_string($db, $_POST['password']);
